4f7c07d093d90ea8af5af78c94f46b55b4d5c027
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-06-10 00:01:29 +0600

    daisy ui connected and working

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ function App() {
   return (
     <>
       <div className="text-center text-6xl">Hello React</div>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </>
   );
 }
