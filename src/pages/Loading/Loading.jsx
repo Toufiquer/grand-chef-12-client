02b57053f2050737e58270899559938dc764b899
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-06-10 01:29:00 +0600

    add loading page

---
+++ src/pages/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center w-full h-screen fixed z-50 top-0 left-0 ">
+        <span className="loading loading-dots loading-lg"></span>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
