7100b245f713a72c0c4d2c47bb38022c13a02f34
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-06-11 01:59:16 +0600

    add devider

--- src/components/Instructors/Instructors.jsx
+++ src/components/Instructors/Instructors.jsx
@@ -2,6 +2,10 @@ import SwipeSlider from "../SwipeSlider/SwipeSlider";
 const Instructors = () => {
   return (
     <div className="my-6">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold">Hello there</h1>
+        <p className="">Provident cupiditate voluptatem et in.</p>
+      </div>
       <SwipeSlider />
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
+import { BsFillStarFill } from "react-icons/bs";
 import Hero from "../../components/Hero/Hero";
+import Instructors from "../../components/Instructors/Instructors";
 import TopClasses from "../../components/TopClasses/TopClasses";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <div className="divider">
+        <BsFillStarFill />
+      </div>
       <TopClasses />
+      <div className="divider">
+        <BsFillStarFill />
+      </div>
+      <Instructors />
     </>
   );
 };
