520b818b971584f419ef5094ce1a7d6031e5cf0f
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-06-11 14:45:51 +0600

    update swipe and responsive

--- src/components/SwipeSlider/SwipeSlider.jsx
+++ src/components/SwipeSlider/SwipeSlider.jsx
@@ -2,114 +2,45 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // import required modules
-import { Pagination } from "swiper";
+import { FreeMode, Pagination, Scrollbar } from "swiper";
 import DisplayCenter from "../DisplayCenter/DisplayCenter";
 const SwipeSlider = () => {
   return (
     <div className="">
       <div className="w-full text-center"></div>
       <Swiper
-        spaceBetween={2}
-        slidesPerView={2}
         autoplay={true}
         pagination={{
           dynamicBullets: true,
         }}
-        modules={[Pagination]}
+        breakpoints={{
+          400: { slidesPerView: 1, spaceBetween: 150 },
+          768: { slidesPerView: 2, spaceBetween: 50 },
+        }}
+        freeMode
+        centeredSlides
+        grabCursor
+        centeredSlidesBounds
+        modules={[FreeMode, Scrollbar, Pagination]}
         className="mySwiper"
-        // onSlideChange={() => console.log("slide change")}
-        // onSwiper={(swiper) => console.log(swiper)}
       >
         {/* Slide 1 */}
-        <SwiperSlide>
-          <DisplayCenter>
-            <div className="flex flex-col">
-              <h1 className="text-4xl text-center font-bold mb-2">
-                Hello there
-              </h1>
-              <img
-                src="https://i.ibb.co/BsXpQ0z/sta-je-html.jpg"
-                alt="Logo"
-                loading="lazy"
-              />
-            </div>
-          </DisplayCenter>
-        </SwiperSlide>
-        {/* Slide 1 */}
-        <SwiperSlide>
-          <DisplayCenter>
-            <div className="flex flex-col">
-              <h1 className="text-4xl text-center font-bold mb-2">
-                Hello there
-              </h1>
-              <img
-                src="https://i.ibb.co/BsXpQ0z/sta-je-html.jpg"
-                alt="Logo"
-                loading="lazy"
-              />
-            </div>
-          </DisplayCenter>
-        </SwiperSlide>
-        {/* Slide 1 */}
-        <SwiperSlide>
-          <DisplayCenter>
-            <div className="flex flex-col">
-              <h1 className="text-4xl text-center font-bold mb-2">
-                Hello there
-              </h1>
-              <img
-                src="https://i.ibb.co/BsXpQ0z/sta-je-html.jpg"
-                alt="Logo"
-                loading="lazy"
-              />
-            </div>
-          </DisplayCenter>
-        </SwiperSlide>
-        {/* Slide 1 */}
-        <SwiperSlide>
-          <DisplayCenter>
-            <div className="flex flex-col">
-              <h1 className="text-4xl text-center font-bold mb-2">
-                Hello there
-              </h1>
-              <img
-                src="https://i.ibb.co/BsXpQ0z/sta-je-html.jpg"
-                alt="Logo"
-                loading="lazy"
-              />
-            </div>
-          </DisplayCenter>
-        </SwiperSlide>
-        {/* Slide 1 */}
-        <SwiperSlide>
-          <DisplayCenter>
-            <div className="flex flex-col">
-              <h1 className="text-4xl text-center font-bold mb-2">
-                Hello there
-              </h1>
-              <img
-                src="https://i.ibb.co/BsXpQ0z/sta-je-html.jpg"
-                alt="Logo"
-                loading="lazy"
-              />
-            </div>
-          </DisplayCenter>
-        </SwiperSlide>
-        {/* Slide 1 */}
-        <SwiperSlide>
-          <DisplayCenter>
-            <div className="flex flex-col">
-              <h1 className="text-4xl text-center font-bold mb-2">
-                Hello there
-              </h1>
-              <img
-                src="https://i.ibb.co/BsXpQ0z/sta-je-html.jpg"
-                alt="Logo"
-                loading="lazy"
-              />
-            </div>
-          </DisplayCenter>
-        </SwiperSlide>
+        {[1, 2, 3, 4, 5, 6, 7, 9, 8]?.map((i) => (
+          <SwiperSlide key={i}>
+            <DisplayCenter>
+              <div className="flex flex-col">
+                <h1 className="text-4xl text-center font-bold mb-2">
+                  Hello there
+                </h1>
+                <img
+                  src="https://i.ibb.co/BsXpQ0z/sta-je-html.jpg"
+                  alt="Logo"
+                  loading="lazy"
+                />
+              </div>
+            </DisplayCenter>
+          </SwiperSlide>
+        ))}
       </Swiper>
       <div className="w-full flex justify-center">
         <div className="btn btn-outline">View More</div>
